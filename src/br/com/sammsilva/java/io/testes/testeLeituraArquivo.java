9fcf39d46f8f263c25e1735f2d9aa91325a81216
Samuel Magalhaes Silva <sammsdev@gmail.com>
2020-01-16 21:53:02 -0300

    First application - java io

--- src/br/com/sammsilva/java/io/testes/testeLeituraArquivo.java
+++ src/br/com/sammsilva/java/io/testes/testeLeituraArquivo.java
@@ -1,9 +1,22 @@
 package br.com.sammsilva.java.io.testes;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class testeLeituraArquivo {
 
 
-
-
-
+    public static void main(String[] args) throws IOException {
+        //Fluxo de Entrada com arquivo // retorna binario // precisamos de outra classe para tratar os dados recebidos
+        FileInputStream fis = new FileInputStream("lorem.txt");
+        //Essa classe recebe o fluxo de entrada, mas ainda le apenas character por character
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+        String linha = br.readLine();
+        System.out.println("linha = " + linha);
+        br.close();
+    }
 }
+

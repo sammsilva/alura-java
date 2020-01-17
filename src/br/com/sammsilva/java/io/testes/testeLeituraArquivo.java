4aaf1b9e8cd48e1ec92f7b391337df4e2ed5b088
Samuel Magalhaes Silva <sammsdev@gmail.com>
2020-01-16 22:04:23 -0300

    Aprendendo sobre InputStream/Readers implementando laço que lê arquivo.txt inteiro

--- src/br/com/sammsilva/java/io/testes/testeLeituraArquivo.java
+++ src/br/com/sammsilva/java/io/testes/testeLeituraArquivo.java
@@ -15,7 +15,11 @@ public class testeLeituraArquivo {
         InputStreamReader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
         String linha = br.readLine();
-        System.out.println("linha = " + linha);
+        while (linha != null){
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+
         br.close();
     }
 }

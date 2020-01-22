51175c0962ad83365750f0082fad5dbdf9cb95d7
Samuel Magalhaes Silva <sammsdev@gmail.com>
2020-01-21 21:31:45 -0300

    Aprendendo sobre InputStream/Readers Lendo e escrevendo arquivos txt

--- src/br/com/sammsilva/java/io/testes/testeLeituraArquivo.java
+++ src/br/com/sammsilva/java/io/testes/testeLeituraArquivo.java
@@ -1,18 +1,18 @@
 package br.com.sammsilva.java.io.testes;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class testeLeituraArquivo {
 
 
     public static void main(String[] args) throws IOException {
+        /**
+         * ENTRADA E LEITURA DE ARQUIVOS
+         * */
         //Fluxo de Entrada com arquivo // retorna binario // precisamos de outra classe para tratar os dados recebidos
-        FileInputStream fis = new FileInputStream("lorem.txt");
+        InputStream fis = new FileInputStream("lorem.txt");
         //Essa classe recebe o fluxo de entrada, mas ainda le apenas character por character
-        InputStreamReader isr = new InputStreamReader(fis);
+        Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
         String linha = br.readLine();
         while (linha != null){
@@ -21,6 +21,47 @@ public class testeLeituraArquivo {
         }
 
         br.close();
+
+        /**
+         * SAIDA E ESCRITA DE ARQUIVOS
+         **/
+
+        OutputStream fos = new FileOutputStream("lorem2.txt");
+        Writer osr = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osr);
+        bw.write("testando escrita no arquivo");
+        bw.newLine();
+        bw.newLine();
+        bw.write("testando escrita no arquivo 2");
+        bw.close();
+
+
+        /**
+         * Ler teclado e guardar no arquivo lorem3.txt
+         * */
+
+        InputStream is = System.in;
+        Reader ris = new InputStreamReader(is);
+        BufferedReader brs = new BufferedReader(ris);
+
+        OutputStream os = new FileOutputStream("lorem3.txt");
+        Writer wos = new OutputStreamWriter(os);
+        BufferedWriter bws = new BufferedWriter(wos);
+
+        String newLine = brs.readLine();
+        //System.out.println("newLine = " + newLine);
+
+        while(newLine!= null && !newLine.isEmpty()) {
+            bws.write(newLine);
+            bws.newLine();
+            bws.flush();
+            newLine = brs.readLine();
+        }
+
+        System.out.println("fechando arquivo...");
+        bws.close();
+        brs.close();
+
     }
 }
 
